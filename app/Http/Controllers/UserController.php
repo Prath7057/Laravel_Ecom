0c69510002169038ebe5440a8a0f8ea015b26f99
Prath@7057 <iamprathpatil@gmail.com>
2024-12-29 20:49:02 +0530

    added signin controller code

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,8 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-   public function signUpData(Request $request) {
+    public function signUpData(Request $request)
+    {
         $validated = $request->validate([
             'user_username' => 'required|min:4|regex:/[A-Z]/|regex:/[0-9]/',
             'user_password' => 'required|min:6|regex:/[A-Z]/|regex:/[0-9]/|regex:/[\W_]/',
@@ -33,9 +34,35 @@ class UserController extends Controller
         $user_id = $user->user_id;
         //
         return redirect()->route('signin')
-        ->with([
-            'success' => 'Registration Successfull!',
-            'user_username' => $validated['user_username']
+            ->with([
+                'success' => 'Registration Successfull!',
+                'user_username' => $validated['user_username']
+            ]);
+    }
+    //
+    public function signInData(Request $request)
+    {
+        $validated = $request->validate([
+            'user_username' => 'required|min:4|regex:/[A-Z]/|regex:/[0-9]/',
+            'user_password' => 'required|min:6|regex:/[A-Z]/|regex:/[0-9]/|regex:/[\W_]/',
+        ], [
+            'user_username.required' => 'The username field is required.',
+            'user_username.min' => 'The username must be at least 4 characters long.',
+            'user_username.regex' => 'The username must contain at least one uppercase letter and one number.',
+            'user_password.required' => 'The password field is required.',
+            'user_password.min' => 'The password must be at least 6 characters long.',
+            'user_password.regex' => 'The password must contain at least one uppercase letter, one number, and one special character.',
         ]);
-   }
+        //
+        $validated['user_ipassword'] = $validated['user_password'];
+        $validated['user_password'] = Hash::make($validated['user_password']);
+        $user = User::create($validated);
+        $user_id = $user->user_id;
+        //
+        return redirect()->route('signin')
+            ->with([
+                'success' => 'Registration Successfull!',
+                'user_username' => $validated['user_username']
+            ]);
+    }
 }
